ebf8ac7d2fdbaa20bad87b73d73080583a5b06a7
ahmedkenore <73717916+ahmedkenore@users.noreply.github.com>
2026-02-26 06:32:33 +0100

    Update test_structureOfClass.py

--- test_structureOfClass.py
+++ test_structureOfClass.py
@@ -5,6 +5,6 @@ class ClasseDeTest(unittest.TestCase):
 
     def test_simple(self):
         self.assertTrue(True)
-#Pour exécuter une classe de test, il faut utiliser la fonction unittest.main()
+#Pour exécuter une classe de test, il faut utiliser la fonction unittest.main() 
 if __name__ == '__main__':
     unittest.main()
