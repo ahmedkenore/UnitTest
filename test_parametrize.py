5c14c1ff4ab9e234b3a01fb5b05c188b175120fc
ahmedkenore <73717916+ahmedkenore@users.noreply.github.com>
2026-02-26 06:31:48 +0100

    Update test_parametrize.py

--- test_parametrize.py
+++ test_parametrize.py
@@ -1,5 +1,6 @@
 import pytest
-#Ici on teste la notion de parametrize
+#Ici on teste la notion de parametrize 
 @pytest.mark.parametrize("num, output",[(1,11),(2,22),(3,35),(4,44)])
 def test_multiplication_11(num, output):
    assert 11*num == output
+
